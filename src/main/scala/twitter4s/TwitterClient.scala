7e60192f82f45f4c80333a9f4224a44fd5f6ad34
Daniela Sfregola <daniela.sfregola@gmail.com>
2015-10-07 06:56:35 +0100

    Added Frieendship to the Twitter Client

--- src/main/scala/twitter4s/TwitterClient.scala
+++ src/main/scala/twitter4s/TwitterClient.scala
@@ -7,10 +7,10 @@ import twitter4s.entities.ConsumerToken._
 import twitter4s.entities.{AccessToken, ConsumerToken}
 import twitter4s.http.clients.OAuthClient
 import twitter4s.http.clients.directmessages.TwitterDirectMessageClient
-import twitter4s.http.clients.search.TwitterSearchClient
+import twitter4s.http.clients.friendships.TwitterFriendshipClient
 import twitter4s.http.clients.statuses.TwitterStatusClient
 
 class TwitterClient(val consumerToken: ConsumerToken = ConsumerTokenFromConf, val accessToken: AccessToken = AccessTokenFromConf)
                    (implicit val actorRefFactory: ActorRefFactory = ActorSystem("twitter4s")) extends OAuthClient with Clients
 
-trait Clients extends TwitterStatusClient with TwitterDirectMessageClient with TwitterSearchClient
+trait Clients extends TwitterStatusClient with TwitterDirectMessageClient with TwitterFriendshipClient
