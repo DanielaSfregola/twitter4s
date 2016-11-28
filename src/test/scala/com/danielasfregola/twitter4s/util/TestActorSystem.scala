608cb6895803576763eb0c7368a4ee539b0e29a9
DanielaSfregola <daniela.sfregola@gmail.com>
2016-11-28 19:37:48 +0000

    Now using 1 instance of actor system when running the tests

--- src/test/scala/com/danielasfregola/twitter4s/util/TestActorSystem.scala
+++ src/test/scala/com/danielasfregola/twitter4s/util/TestActorSystem.scala
@@ -5,6 +5,10 @@ import akka.testkit.TestKit
 
 import com.danielasfregola.twitter4s.providers.ActorRefFactoryProvider
 
-abstract class TestActorSystem extends TestKit(ActorSystem()) with ActorRefFactoryProvider {
+abstract class TestActorSystem extends TestKit(TestActorSystem.system) with ActorRefFactoryProvider {
   implicit val actorRefFactory = system
+}
+
+object TestActorSystem {
+  lazy val system = ActorSystem("twitter4s-unit-tests")
 }
