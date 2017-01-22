28f32f881fb36aa94a55fd3bfc08f1c95805cdc5
klachata <klachata@gumtree.com>
2017-01-22 13:18:52 +0000

    add default image size

--- src/test/scala/com/danielasfregola/twitter4s/entities/ProfileImageSpec.scala
+++ src/test/scala/com/danielasfregola/twitter4s/entities/ProfileImageSpec.scala
@@ -1,8 +1,20 @@
 package com.danielasfregola.twitter4s.entities
 
-/**
-  * Created by klachata on 22/01/2017.
-  */
-class ProfileImageSpec {
+import org.specs2.mutable.Specification
+
+class ProfileImageSpec extends Specification {
+
+  "ProfileImage" should {
+
+    "generate 4 type of image sizes from the original picture url string" in {
+      val profile = ProfileImage("test_size.ext")
+
+      profile.default === "text.ext"
+      profile.mini === "text_mini.ext"
+      profile.normal === "text_normal.ext"
+      profile.bigger === "text_bigger.ext"
+    }
+
+  }
 
 }
