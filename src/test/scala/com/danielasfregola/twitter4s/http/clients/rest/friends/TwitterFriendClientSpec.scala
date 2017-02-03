3dbc38021e4a1d7fc9cdc97822522a50e38caf8c
DanielaSfregola <daniela.sfregola@gmail.com>
2017-02-03 21:38:39 +0000

    #80 Added rates support for TwitterFriendClient

--- src/test/scala/com/danielasfregola/twitter4s/http/clients/rest/friends/TwitterFriendClientSpec.scala
+++ src/test/scala/com/danielasfregola/twitter4s/http/clients/rest/friends/TwitterFriendClientSpec.scala
@@ -2,7 +2,7 @@ package com.danielasfregola.twitter4s.http.clients.rest.friends
 
 
 import akka.http.scaladsl.model.HttpMethods
-import com.danielasfregola.twitter4s.entities.{UserIds, UserStringifiedIds, Users}
+import com.danielasfregola.twitter4s.entities.{RatedData, UserIds, UserStringifiedIds, Users}
 import com.danielasfregola.twitter4s.util.rest.ClientSpec
 
 class TwitterFriendClientSpec extends ClientSpec {
@@ -12,57 +12,63 @@ class TwitterFriendClientSpec extends ClientSpec {
   "Twitter Friend Client" should {
 
     "get friends ids of a specific user by id" in new TwitterFriendClientSpecContext {
-      val result: UserIds = when(friendIdsForUserId(2911461333L)).expectRequest { request =>
+      val result: RatedData[UserIds] = when(friendIdsForUserId(2911461333L)).expectRequest { request =>
         request.method === HttpMethods.GET
         request.uri.endpoint === "https://api.twitter.com/1.1/friends/ids.json"
         request.uri.queryString() === Some("count=5000&cursor=-1&stringify_ids=false&user_id=2911461333")
-      }.respondWith("/twitter/rest/friends/friends_ids.json").await
-      result === loadJsonAs[UserIds]("/fixtures/rest/friends/friends_ids.json")
+      }.respondWithRated("/twitter/rest/friends/friends_ids.json").await
+      result.rate_limit === rateLimit
+      result.data === loadJsonAs[UserIds]("/fixtures/rest/friends/friends_ids.json")
     }
 
     "get friends ids of a specific user by name" in new TwitterFriendClientSpecContext {
-      val result: UserIds = when(friendIdsForUser("DanielaSfregola")).expectRequest { request =>
+      val result: RatedData[UserIds] = when(friendIdsForUser("DanielaSfregola")).expectRequest { request =>
         request.method === HttpMethods.GET
         request.uri.endpoint === "https://api.twitter.com/1.1/friends/ids.json"
         request.uri.queryString() === Some("count=5000&cursor=-1&screen_name=DanielaSfregola&stringify_ids=false")
-      }.respondWith("/twitter/rest/friends/friends_ids.json").await
-      result === loadJsonAs[UserIds]("/fixtures/rest/friends/friends_ids.json")
+      }.respondWithRated("/twitter/rest/friends/friends_ids.json").await
+      result.rate_limit === rateLimit
+      result.data === loadJsonAs[UserIds]("/fixtures/rest/friends/friends_ids.json")
     }
 
     "get friends stringified ids of a specific user by id" in new TwitterFriendClientSpecContext {
-      val result: UserStringifiedIds = when(friendStringifiedIdsForUserId(2911461333L)).expectRequest { request =>
+      val result: RatedData[UserStringifiedIds] = when(friendStringifiedIdsForUserId(2911461333L)).expectRequest { request =>
         request.method === HttpMethods.GET
         request.uri.endpoint === "https://api.twitter.com/1.1/friends/ids.json"
         request.uri.queryString() === Some("count=5000&cursor=-1&stringify_ids=true&user_id=2911461333")
-      }.respondWith("/twitter/rest/friends/friends_ids_stringified.json").await
-      result === loadJsonAs[UserStringifiedIds]("/fixtures/rest/friends/friends_ids_stringified.json")
+      }.respondWithRated("/twitter/rest/friends/friends_ids_stringified.json").await
+      result.rate_limit === rateLimit
+      result.data === loadJsonAs[UserStringifiedIds]("/fixtures/rest/friends/friends_ids_stringified.json")
     }
 
     "get friends stringified ids of a specific user by name" in new TwitterFriendClientSpecContext {
-      val result: UserStringifiedIds = when(friendStringifiedIdsForUser("DanielaSfregola")).expectRequest { request =>
+      val result: RatedData[UserStringifiedIds] = when(friendStringifiedIdsForUser("DanielaSfregola")).expectRequest { request =>
         request.method === HttpMethods.GET
         request.uri.endpoint === "https://api.twitter.com/1.1/friends/ids.json"
         request.uri.queryString() === Some("count=5000&cursor=-1&screen_name=DanielaSfregola&stringify_ids=true")
-      }.respondWith("/twitter/rest/friends/friends_ids_stringified.json").await
-      result === loadJsonAs[UserStringifiedIds]("/fixtures/rest/friends/friends_ids_stringified.json")
+      }.respondWithRated("/twitter/rest/friends/friends_ids_stringified.json").await
+      result.rate_limit === rateLimit
+      result.data === loadJsonAs[UserStringifiedIds]("/fixtures/rest/friends/friends_ids_stringified.json")
     }
 
     "get friends of a user" in new TwitterFriendClientSpecContext {
-      val result: Users = when(friendsForUser("DanielaSfregola", count = 10)).expectRequest { request =>
+      val result: RatedData[Users] = when(friendsForUser("DanielaSfregola", count = 10)).expectRequest { request =>
         request.method === HttpMethods.GET
         request.uri.endpoint === "https://api.twitter.com/1.1/friends/list.json"
         request.uri.queryString() === Some("count=10&cursor=-1&include_user_entities=true&screen_name=DanielaSfregola&skip_status=false")
-      }.respondWith("/twitter/rest/friends/users.json").await
-      result === loadJsonAs[Users]("/fixtures/rest/friends/users.json")
+      }.respondWithRated("/twitter/rest/friends/users.json").await
+      result.rate_limit === rateLimit
+      result.data === loadJsonAs[Users]("/fixtures/rest/friends/users.json")
     }
 
     "get friends of a user by user id" in new TwitterFriendClientSpecContext {
-      val result: Users = when(friendsForUserId(2911461333L, count = 10)).expectRequest { request =>
+      val result: RatedData[Users] = when(friendsForUserId(2911461333L, count = 10)).expectRequest { request =>
         request.method === HttpMethods.GET
         request.uri.endpoint === "https://api.twitter.com/1.1/friends/list.json"
         request.uri.queryString() === Some("count=10&cursor=-1&include_user_entities=true&skip_status=false&user_id=2911461333")
-      }.respondWith("/twitter/rest/friends/users.json").await
-      result === loadJsonAs[Users]("/fixtures/rest/friends/users.json")
+      }.respondWithRated("/twitter/rest/friends/users.json").await
+      result.rate_limit === rateLimit
+      result.data === loadJsonAs[Users]("/fixtures/rest/friends/users.json")
     }
 
   }
