ad5be9b1a71f37773122eb09db7dc95a48a9ef30
Daniela Sfregola <daniela.sfregola@gmail.com>
2017-03-05 19:35:18 +0000

    #42 Added OAuthClient.authenticationUrl

--- src/test/scala/com/danielasfregola/twitter4s/http/clients/authentication/oauth/TwitterOAuthClientSpec.scala
+++ src/test/scala/com/danielasfregola/twitter4s/http/clients/authentication/oauth/TwitterOAuthClientSpec.scala
@@ -7,11 +7,11 @@ import com.danielasfregola.twitter4s.helpers.ClientSpec
 
 class TwitterOAuthClientSpec extends ClientSpec {
 
-  class TwitterAccountClientSpecContext extends AuthenticationClientSpecContext with TwitterOAuthClient
+  class TwitterOAuthClientSpecContext extends AuthenticationClientSpecContext with TwitterOAuthClient
 
   "Twitter OAuth Client" should {
 
-    "request a token with static callback" in new TwitterAccountClientSpecContext {
+    "request a token with static callback" in new TwitterOAuthClientSpecContext {
       val result: OAuthToken = when(requestToken()).expectRequest { request =>
         request.method === HttpMethods.POST
         request.uri.endpoint === "https://api.twitter.com/oauth/request_token"
@@ -24,7 +24,7 @@ class TwitterOAuthClientSpec extends ClientSpec {
       result === expectedResult
     }
 
-    "request a token with dynamic callback and access type" in new TwitterAccountClientSpecContext {
+    "request a token with dynamic callback and access type" in new TwitterOAuthClientSpecContext {
       val result: OAuthToken = when(requestToken(Some("http://my.callback"), Some(AccessType.Read))).expectRequest { request =>
         request.method === HttpMethods.POST
         request.uri.endpoint === "https://api.twitter.com/oauth/request_token"
@@ -35,6 +35,12 @@ class TwitterOAuthClientSpec extends ClientSpec {
         oauth_token_secret = "Kd75W4OQfb2oJTV0vzGzeXftVAwgMnEK9MumzYcM",
         oauth_callback_confirmed = true)
       result === expectedResult
+    }
+
+    "generate an authentication url" in new TwitterOAuthClientSpecContext {
+      val url: String = authenticationUrl(oauth_token = "my-auth-token", force_login = true, screen_name = Some("DanielaSfregola"))
+      val params = "oauth_token=my-auth-token&force_login=true&screen_name=DanielaSfregola"
+      url === s"https://api.twitter.com/oauth/authenticate?$params"
     }
   }
 }
