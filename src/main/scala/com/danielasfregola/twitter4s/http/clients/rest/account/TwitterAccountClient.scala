d8fa9a4a04ea88afdb84e7b5727add5799ae5a6b
Anurag Pandey <32086168+lapmid@users.noreply.github.com>
2017-10-17 14:08:24 +0530

    Fixed all links on this page

--- src/main/scala/com/danielasfregola/twitter4s/http/clients/rest/account/TwitterAccountClient.scala
+++ src/main/scala/com/danielasfregola/twitter4s/http/clients/rest/account/TwitterAccountClient.scala
@@ -33,8 +33,8 @@ trait TwitterAccountClient {
 
   /** Updates the authenticating user’s settings.
     * For more information see
-    * <a href="https://dev.twitter.com/rest/reference/post/account/settings" target="_blank">
-    *   https://dev.twitter.com/rest/reference/post/account/settings</a>.
+    * <a href="https://developer.twitter.com/en/docs/accounts-and-users/manage-account-settings/api-reference/post-account-remove_profile_banner" target="_blank">
+    *   https://developer.twitter.com/en/docs/accounts-and-users/manage-account-settings/api-reference/post-account-remove_profile_banner</a>.
     *
     * @param allow_contributor_request : Optional, by default it is `None`.
     *                                  Whether to allow others to include user as contributor.
@@ -72,8 +72,8 @@ trait TwitterAccountClient {
 
   /** Updates the authenticating user’s settings.
     * For more information see
-    * <a href="https://dev.twitter.com/rest/reference/post/account/settings" target="_blank">
-    *   https://dev.twitter.com/rest/reference/post/account/settings</a>.
+    * <a href="https://developer.twitter.com/en/docs/accounts-and-users/manage-account-settings/api-reference/post-account-remove_profile_banner" target="_blank">
+    *   https://developer.twitter.com/en/docs/accounts-and-users/manage-account-settings/api-reference/post-account-remove_profile_banner</a>.
     *
     * @param settings_options : The setting options to update. Only the parameters specified will be updated.
     * @return : The updated settings.
@@ -108,8 +108,8 @@ trait TwitterAccountClient {
 
   /** Sets the name that users are able to set under the “Account” tab of their settings page.
     * For more information see
-    * <a href="https://dev.twitter.com/rest/reference/post/account/update_profile" target="_blank">
-    *   https://dev.twitter.com/rest/reference/post/account/update_profile</a>.
+    * <a href="https://developer.twitter.com/en/docs/accounts-and-users/manage-account-settings/api-reference/post-account-update_profile" target="_blank">
+    *   https://developer.twitter.com/en/docs/accounts-and-users/manage-account-settings/api-reference/post-account-update_profile</a>.
     *
     * @param name : Full name associated with the profile. Maximum of 20 characters.
     * @param include_entities : By default it is `true`.
@@ -127,8 +127,8 @@ trait TwitterAccountClient {
 
   /** Sets the url that users are able to set under the “Account” tab of their settings page.
     * For more information see
-    * <a href="https://dev.twitter.com/rest/reference/post/account/update_profile" target="_blank">
-    *   https://dev.twitter.com/rest/reference/post/account/update_profile</a>.
+    * <a href="https://developer.twitter.com/en/docs/accounts-and-users/manage-account-settings/api-reference/post-account-update_profile" target="_blank">
+    *   https://developer.twitter.com/en/docs/accounts-and-users/manage-account-settings/api-reference/post-account-update_profile</a>.
     *
     * @param url : URL associated with the profile. Will be prepended with “http://” if not present.
     *            Maximum of 100 characters.
@@ -147,8 +147,8 @@ trait TwitterAccountClient {
 
   /** Sets the location that users are able to set under the “Account” tab of their settings page.
     * For more information see
-    * <a href="https://dev.twitter.com/rest/reference/post/account/update_profile" target="_blank">
-    *   https://dev.twitter.com/rest/reference/post/account/update_profile</a>.
+    * <a href="https://developer.twitter.com/en/docs/accounts-and-users/manage-account-settings/api-reference/post-account-update_profile" target="_blank">
+    *   https://developer.twitter.com/en/docs/accounts-and-users/manage-account-settings/api-reference/post-account-update_profile</a>.
     *
     * @param location : The city or country describing where the user of the account is located.
     *                 The contents are not normalized or geocoded in any way. Maximum of 30 characters.
@@ -167,8 +167,8 @@ trait TwitterAccountClient {
 
   /** Sets the description that users are able to set under the “Account” tab of their settings page.
     * For more information see
-    * <a href="https://dev.twitter.com/rest/reference/post/account/update_profile" target="_blank">
-    *   https://dev.twitter.com/rest/reference/post/account/update_profile</a>.
+    * <a href="https://developer.twitter.com/en/docs/accounts-and-users/manage-account-settings/api-reference/post-account-update_profile" target="_blank">
+    *   https://developer.twitter.com/en/docs/accounts-and-users/manage-account-settings/api-reference/post-account-update_profile</a>.
     *
     * @param description : A description of the user owning the account. Maximum of 160 characters.
     * @param include_entities : By default it is `true`.
@@ -186,8 +186,8 @@ trait TwitterAccountClient {
 
   /** Sets the link color that users are able to set under the “Account” tab of their settings page.
     * For more information see
-    * <a href="https://dev.twitter.com/rest/reference/post/account/update_profile" target="_blank">
-    *   https://dev.twitter.com/rest/reference/post/account/update_profile</a>.
+    * <a href="https://developer.twitter.com/en/docs/accounts-and-users/manage-account-settings/api-reference/post-account-update_profile" target="_blank">
+    *   https://developer.twitter.com/en/docs/accounts-and-users/manage-account-settings/api-reference/post-account-update_profile</a>.
     *
     * @param link_color : Sets a hex value that controls the color scheme of links used on the authenticating user’s profile page on twitter.com.
     *                   This must be a valid hexadecimal value, and may be either three or six characters (ex: F00 or FF0000).
@@ -206,8 +206,8 @@ trait TwitterAccountClient {
 
   /** Sets some values that users are able to set under the “Account” tab of their settings page. Only the parameters specified will be updated.
     * For more information see
-    * <a href="https://dev.twitter.com/rest/reference/post/account/update_profile" target="_blank">
-    *   https://dev.twitter.com/rest/reference/post/account/update_profile</a>.
+    * <a href="https://developer.twitter.com/en/docs/accounts-and-users/manage-account-settings/api-reference/post-account-update_profile" target="_blank">
+    *   https://developer.twitter.com/en/docs/accounts-and-users/manage-account-settings/api-reference/post-account-update_profile</a>.
     *
     * @param update : The profile values to update.
     * @return : The user representation.
@@ -219,8 +219,8 @@ trait TwitterAccountClient {
 
   /** Removes the uploaded profile banner for the authenticating user.
     * For more information see
-    * <a href="https://dev.twitter.com/rest/reference/post/account/remove_profile_banner" target="_blank">
-    *   https://dev.twitter.com/rest/reference/post/account/remove_profile_banner</a>.
+    * <a href="https://developer.twitter.com/en/docs/accounts-and-users/manage-account-settings/api-reference/post-account-remove_profile_banner" target="_blank">
+    *   https://developer.twitter.com/en/docs/accounts-and-users/manage-account-settings/api-reference/post-account-remove_profile_banner</a>.
     * */
   def removeProfileBanner(): Future[Unit] = {
     import restClient._
