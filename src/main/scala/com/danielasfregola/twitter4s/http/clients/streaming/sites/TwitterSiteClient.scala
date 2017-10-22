7890eb0f4cded6de4b8f617a45280cdafeb5400d
Oliver Schrenk <oliver.schrenk@gmail.com>
2017-10-22 15:53:46 +0200

    Change doc links in TwitterSiteClient
    Unfortunately this means loosing direct links to anchors, as they were
    removed in the new documentation.

--- src/main/scala/com/danielasfregola/twitter4s/http/clients/streaming/sites/TwitterSiteClient.scala
+++ src/main/scala/com/danielasfregola/twitter4s/http/clients/streaming/sites/TwitterSiteClient.scala
@@ -17,32 +17,32 @@ trait TwitterSiteClient {
 
   private val siteUrl = s"$siteStreamingTwitterUrl/$twitterVersion"
 
-  /** Starts a streaming connection from Twitter's site API. SStreams messages for a set of users,
-    * as described in <a href="https://dev.twitter.com/streaming/sitestreams" target="_blank">Site streams</a>.
+  /** Starts a streaming connection from Twitter's site API. Streams messages for a set of users,
+    * as described in <a href="https://developer.twitter.com/en/docs/tutorials/consuming-streaming-data" target="_blank">Site streams</a>.
     * The function returns a future of a `TwitterStream` that can be use to close or replace the stream when needed.
     * If there are failures in establishing the initial connection, the Future returned will be completed with a failure.
     * Since it's an asynchronous event stream, all the events will be parsed as entities of type `SiteStreamingMessage`
     * and processed accordingly to the partial function `f`. All the messages that do not match `f` are automatically ignored.
     * For more information see
-    * <a href="https://dev.twitter.com/streaming/reference/get/site" target="_blank">
-    *   https://dev.twitter.com/streaming/reference/get/site</a>.
+    * <a href="https://developer.twitter.com/en/docs/accounts-and-users/subscribe-account-activity/api-reference/site-stream" target="_blank">
+    *   https://developer.twitter.com/en/docs/accounts-and-users/subscribe-account-activity/api-reference/site-stream</a>.
     *
     * @param follow : Empty by default. A comma separated list of user IDs, indicating the users to return statuses for in the stream.
-    *                 For more information <a href="https://dev.twitter.com/streaming/overview/request-parameters#follow" target="_blank">
-    *                   https://dev.twitter.com/streaming/overview/request-parameters#follow</a>
+    *                 For more information <a href="https://developer.twitter.com/en/docs/accounts-and-users/subscribe-account-activity/api-reference/site-stream" target="_blank">
+    *                   https://developer.twitter.com/en/docs/accounts-and-users/subscribe-account-activity/api-reference/site-stream</a>
     * @param with: `User` by default. Specifies whether to return information for just the users specified in the follow parameter, or include messages from accounts they follow.
-    *              For more information see <a href="https://dev.twitter.com/streaming/overview/request-parameters" target="_blank">
-    *                https://dev.twitter.com/streaming/overview/request-parameters</a>
+    *              For more information see <a href="https://developer.twitter.com/en/docs/accounts-and-users/subscribe-account-activity/api-reference/site-stream" target="_blank">
+    *                https://developer.twitter.com/en/docs/accounts-and-users/subscribe-account-activity/api-reference/site-stream</a>
     * @param replies: Optional. By default @replies are only sent if the current user follows both the sender and receiver of the reply.
     *                 To receive all the replies, set the argument to `true`.
-    *                 For more information see <a href="https://dev.twitter.com/streaming/overview/request-parameters#replies" target="_blank">
-    *                   https://dev.twitter.com/streaming/overview/request-parameters#replies</a>
+    *                 For more information see <a href="https://developer.twitter.com/en/docs/accounts-and-users/subscribe-account-activity/api-reference/site-stream" target="_blank">
+    *                   https://developer.twitter.com/en/docs/accounts-and-users/subscribe-account-activity/api-reference/site-stream</a>
     * @param stringify_friend_ids: Optional. Specifies whether to send the Friend List preamble as an array of integers or an array of strings.
-    *                              For more information see <a href="https://dev.twitter.com/streaming/overview/request-parameters#stringify_friend_id" tagert="_blank">
-    *                                https://dev.twitter.com/streaming/overview/request-parameters#stringify_friend_id</a>
+    *                              For more information see <a href="https://developer.twitter.com/en/docs/accounts-and-users/subscribe-account-activity/api-reference/site-stream" tagert="_blank">
+    *                                https://developer.twitter.com/en/docs/accounts-and-users/subscribe-account-activity/api-reference/site-stream</a>
     * @param languages : Empty by default. A comma separated list of 'BCP 47' language identifiers.
-    *                    For more information <a href="https://dev.twitter.com/streaming/overview/request-parameters#language" target="_blank">
-    *                      https://dev.twitter.com/streaming/overview/request-parameters#language</a>
+    *                    For more information <a href="https://developer.twitter.com/en/docs/tweets/filter-realtime/guides/basic-stream-parameters" target="_blank">
+    *                      https://developer.twitter.com/en/docs/tweets/filter-realtime/guides/basic-stream-parameters</a>
     * @param stall_warnings : Default to false. Specifies whether stall warnings (`WarningMessage`) should be delivered as part of the updates.
     * @param f: the function that defines how to process the received messages
     */
