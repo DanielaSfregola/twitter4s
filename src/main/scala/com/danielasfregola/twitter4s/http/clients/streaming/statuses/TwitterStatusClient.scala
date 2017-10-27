9b38b1024af18c0e2c5eddce8fd9622b8cbe3781
Allan Timothy Leong <allantleong@gmail.com>
2017-10-27 18:42:56 +0800

    Fix location filter parameters for streaming client

--- src/main/scala/com/danielasfregola/twitter4s/http/clients/streaming/statuses/TwitterStatusClient.scala
+++ src/main/scala/com/danielasfregola/twitter4s/http/clients/streaming/statuses/TwitterStatusClient.scala
@@ -1,6 +1,8 @@
 package com.danielasfregola.twitter4s
 package http.clients.streaming.statuses
 
+import com.danielasfregola.twitter4s.entities.GeoBoundingBox
+import com.danielasfregola.twitter4s.entities.GeoBoundingBox.toLngLatPairs
 import com.danielasfregola.twitter4s.entities.enums.FilterLevel
 import com.danielasfregola.twitter4s.entities.enums.FilterLevel.FilterLevel
 import com.danielasfregola.twitter4s.entities.enums.Language.Language
@@ -48,15 +50,13 @@ trait TwitterStatusClient {
     */
   def filterStatuses(follow: Seq[Long] = Seq.empty,
                      tracks: Seq[String] = Seq.empty,
-                     locations: Seq[Double] = Seq.empty,
+                     locations: Seq[GeoBoundingBox] = Seq.empty,
                      languages: Seq[Language] = Seq.empty,
                      stall_warnings: Boolean = false,
-                     filter_level: FilterLevel = FilterLevel.None)(
-      f: PartialFunction[CommonStreamingMessage, Unit]): Future[TwitterStream] = {
+                     filter_level: FilterLevel = FilterLevel.None)(f: PartialFunction[CommonStreamingMessage, Unit]): Future[TwitterStream] = {
     import streamingClient._
-    require(follow.nonEmpty || tracks.nonEmpty || locations.nonEmpty,
-            "At least one of 'follow', 'tracks' or 'locations' needs to be non empty")
-    val filters = StatusFilters(follow, tracks, locations, languages, stall_warnings, filter_level)
+    require(follow.nonEmpty || tracks.nonEmpty || locations.nonEmpty, "At least one of 'follow', 'tracks' or 'locations' needs to be non empty")
+    val filters = StatusFilters(follow, tracks, toLngLatPairs(locations), languages, stall_warnings, filter_level)
     preProcessing()
     Post(s"$statusUrl/filter.json", filters).processStream(f)
   }
@@ -86,8 +86,8 @@ trait TwitterStatusClient {
   def sampleStatuses(languages: Seq[Language] = Seq.empty,
                      stall_warnings: Boolean = false,
                      tracks: Seq[String] = Seq.empty,
-                     filter_level: FilterLevel = FilterLevel.None)(
-      f: PartialFunction[CommonStreamingMessage, Unit]): Future[TwitterStream] = {
+                     filter_level: FilterLevel = FilterLevel.None)
+                    (f: PartialFunction[CommonStreamingMessage, Unit]): Future[TwitterStream] = {
     import streamingClient._
     val parameters = StatusSampleParameters(languages, stall_warnings, tracks, filter_level)
     preProcessing()
@@ -113,10 +113,10 @@ trait TwitterStatusClient {
     * @param stall_warnings : Default to false. Specifies whether stall warnings (`WarningMessage`) should be delivered as part of the updates.
     * @param f : Function that defines how to process the received messages.
     */
-  def firehoseStatuses(
-      count: Option[Int] = None,
-      languages: Seq[Language] = Seq.empty,
-      stall_warnings: Boolean = false)(f: PartialFunction[CommonStreamingMessage, Unit]): Future[TwitterStream] = {
+  def firehoseStatuses(count: Option[Int] = None,
+                       languages: Seq[Language] = Seq.empty,
+                       stall_warnings: Boolean = false)
+                      (f: PartialFunction[CommonStreamingMessage, Unit]): Future[TwitterStream] = {
     import streamingClient._
     val maxCount = 150000
     require(Math.abs(count.getOrElse(0)) <= maxCount, s"count must be between -$maxCount and +$maxCount")
--- src/test/scala/com/danielasfregola/twitter4s/http/clients/streaming/users/TwitterUserClientSpec.scala
+++ src/test/scala/com/danielasfregola/twitter4s/http/clients/streaming/users/TwitterUserClientSpec.scala
@@ -1,6 +1,7 @@
 package com.danielasfregola.twitter4s.http.clients.streaming.users
 
 import akka.http.scaladsl.model.HttpMethods
+import com.danielasfregola.twitter4s.entities.{GeoBoundingBox, Coordinate}
 import com.danielasfregola.twitter4s.entities.enums.Language
 import com.danielasfregola.twitter4s.helpers.ClientSpec
 
@@ -11,13 +12,18 @@ class TwitterUserClientSpec extends ClientSpec {
   "Twitter User Streaming Client" should {
 
     "start a filtered user stream" in new TwitterUserClientSpecContext {
+      val locationsFilter = Seq(
+        GeoBoundingBox(Coordinate(-122.75, 36.8), Coordinate(-121.75, 37.8)),
+        GeoBoundingBox(Coordinate(-74, 40), Coordinate(-73, 41))
+      )
       val result: Unit =
-        when(userEvents(tracks = Seq("trending"), languages = Seq(Language.English))(dummyProcessing))
+        when(userEvents(tracks = Seq("trending"),
+                        languages = Seq(Language.English),
+                        locations = locationsFilter)(dummyProcessing))
           .expectRequest { request =>
             request.method === HttpMethods.GET
             request.uri.endpoint === "https://userstream.twitter.com/1.1/user.json"
-            request.uri.queryString() === Some(
-              "filter_level=none&language=en&stall_warnings=false&stringify_friend_ids=false&track=trending&with=followings")
+            request.uri.queryString() === Some("filter_level=none&language=en&locations=-122.75,36.8,-121.75,37.8,-74.0,40.0,-73.0,41.0&stall_warnings=false&stringify_friend_ids=false&track=trending&with=followings")
           }
           .respondWithOk
           .await
