17c221608d84501e957d2168431166b37763153e
Allan Timothy Leong <allantleong@gmail.com>
2017-10-27 18:42:56 +0800

    Fix location filter parameters for streaming client

--- src/test/scala/com/danielasfregola/twitter4s/http/marshalling/BodyEncoderSpec.scala
+++ src/test/scala/com/danielasfregola/twitter4s/http/marshalling/BodyEncoderSpec.scala
@@ -7,19 +7,20 @@ class BodyEncoderSpec extends Specification with BodyEncoder {
   "BodyEncoder" should {
 
     "encode a case class to a body with encoded params" in {
-      case class TestData(d: Seq[Long], c: String, b: Option[Boolean], a: Int)
+      case class TestData(e: Seq[(Double, Double)], d: Seq[Long], c: String, b: Option[Boolean], a: Int)
 
-      val test = TestData(Seq(1,2,3),"Hello Ladies + Gentlemen, a signed OAuth request!", None, 5)
+      val test = TestData(Seq((1,2), (3,4)), Seq(1,2,3),"Hello Ladies + Gentlemen, a signed OAuth request!", None, 5)
       val result = toBodyAsEncodedParams(test)
-      result === "a=5&c=Hello+Ladies+%2B+Gentlemen%2C+a+signed+OAuth+request%21&d=1%2C2%2C3"
+      println(result)
+      result === "a=5&c=Hello+Ladies+%2B+Gentlemen%2C+a+signed+OAuth+request%21&d=1%2C2%2C3&e=1.0%2C2.0%2C3.0%2C4.0"
     }
 
     "encode a case class to a body with params" in {
-      case class TestData(d: Seq[Long], c: String, b: Option[Boolean], a: Int)
+      case class TestData(e: Seq[(Double, Double)], d: Seq[Long], c: String, b: Option[Boolean], a: Int)
 
-      val test = TestData(Seq(1,2,3),"Hello Ladies + Gentlemen, a signed OAuth request!", None, 5)
+      val test = TestData(Seq((1,2)), Seq(1,2,3),"Hello Ladies + Gentlemen, a signed OAuth request!", None, 5)
       val result = toBodyAsParams(test)
-      result === "a=5&c=Hello Ladies + Gentlemen, a signed OAuth request!&d=1,2,3"
+      result === "a=5&c=Hello Ladies + Gentlemen, a signed OAuth request!&d=1,2,3&e=1.0,2.0"
     }
 
   }
