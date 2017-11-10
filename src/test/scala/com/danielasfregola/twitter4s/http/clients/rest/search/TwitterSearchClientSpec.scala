78115be408312c5f9476de7cb04e0c31fdacb5de
valeriybaibossynov <vbaibossynov@evolutiongaming.com>
2017-11-10 12:23:04 +0200

    Reporoduc issue #174

--- src/test/scala/com/danielasfregola/twitter4s/http/clients/rest/search/TwitterSearchClientSpec.scala
+++ src/test/scala/com/danielasfregola/twitter4s/http/clients/rest/search/TwitterSearchClientSpec.scala
@@ -19,6 +19,17 @@ class TwitterSearchClientSpec extends ClientSpec {
       result.rate_limit === rateLimit
       result.data === loadJsonAs[StatusSearch]("/fixtures/rest/search/tweets.json")
     }
+
+    "search for tweets with url in it" in new TwitterSearchClientSpecContext {
+      val result: RatedData[StatusSearch] = when(searchTweet(
+        "https://github.com/ReactiveX/RxJava")).expectRequest { request =>
+        request.method === HttpMethods.GET
+        request.uri.endpoint === "https://api.twitter.com/1.1/search/tweets.json"
+        request.uri.rawQueryString === Some("count=15&include_entities=true" +
+          "&q=https%3A%2F%2Fgithub.com%2FReactiveX%2FRxJava&result_type=mixed")
+      }.respondWithRated("/twitter/rest/search/tweets.json").await
+
+    }
   }
 
 }
