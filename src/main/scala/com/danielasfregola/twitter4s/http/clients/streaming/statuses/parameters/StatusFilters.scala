261d9d886d2c4c9fb50c0176b3df9d6cd579b6f2
Daniela Sfregola <daniela.sfregola@gmail.com>
2018-02-13 15:59:09 +0000

    Adjusting version and formatting after rebase

--- src/main/scala/com/danielasfregola/twitter4s/http/clients/streaming/statuses/parameters/StatusFilters.scala
+++ src/main/scala/com/danielasfregola/twitter4s/http/clients/streaming/statuses/parameters/StatusFilters.scala
@@ -4,8 +4,8 @@ import com.danielasfregola.twitter4s.entities.enums.FilterLevel.FilterLevel
 import com.danielasfregola.twitter4s.entities.enums.Language.Language
 
 private[twitter4s] final case class StatusFilters(follow: Seq[Long],
-                                            track: Seq[String],
-                                            locations: Seq[(Double, Double)],
-                                            language: Seq[Language],
-                                            stall_warnings: Boolean,
-                                            filter_level: FilterLevel)
+                                                  track: Seq[String],
+                                                  locations: Seq[(Double, Double)],
+                                                  language: Seq[Language],
+                                                  stall_warnings: Boolean,
+                                                  filter_level: FilterLevel)
