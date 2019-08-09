ecfe3371c1af33c7ce70139371115719a5398892
Vladimir Penkov <penkov.vladimir@gmail.com>
2019-08-09 21:57:44 +0300

    Update ErrorHandler.scala

--- src/main/scala/com/danielasfregola/twitter4s/http/clients/streaming/ErrorHandler.scala
+++ src/main/scala/com/danielasfregola/twitter4s/http/clients/streaming/ErrorHandler.scala
@@ -1,6 +1,7 @@
 package com.danielasfregola.twitter4s.http.clients.streaming
+import com.typesafe.scalalogging.LazyLogging
 
-object ErrorHandler extends LazyLogging{
+object ErrorHandler extends LazyLogging {
   def ignore: PartialFunction[Throwable, Unit] = {
     case scala.util.control.NonFatal(e) => logger.warn("Default error handling", e)
   }
