d11a62c1be74a7908cd3987faac5d0c44da7d767
Denis Yerusalimtsev <33877040+denisyerusalimtsev@users.noreply.github.com>
2021-07-29 16:47:47 +0300

    Resolve typo in PlaceFields enum
    Resolve typo in PlaceFields enum

--- src/main/scala/com/danielasfregola/twitter4s/entities/v2/enums/fields/PlaceFields.scala
+++ src/main/scala/com/danielasfregola/twitter4s/entities/v2/enums/fields/PlaceFields.scala
@@ -3,7 +3,7 @@ package com.danielasfregola.twitter4s.entities.v2.enums.fields
 object PlaceFields extends Enumeration {
   type PlaceFields = Value
 
-  val ContainedWidth = Value("contained_within")
+  val ContainedWithin = Value("contained_within")
   val Country = Value("country")
   val CountryCode = Value("country_code")
   val FullName = Value("full_name")
