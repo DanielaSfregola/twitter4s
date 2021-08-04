94838324fc5916a43d22b72a2fb371ee7497df3c
Harry Wada <harry.wada@bazaarvoice.com>
2021-08-04 14:56:10 -0500

    Replace Integers with Ints

--- src/main/scala/com/danielasfregola/twitter4s/entities/v2/Media.scala
+++ src/main/scala/com/danielasfregola/twitter4s/entities/v2/Media.scala
@@ -2,33 +2,33 @@ package com.danielasfregola.twitter4s.entities.v2
 
 final case class Media(media_key: String,
                        `type`: String,
-                       duration_ms: Option[Integer],
-                       height: Option[Integer],
+                       duration_ms: Option[Int],
+                       height: Option[Int],
                        non_public_metrics: Option[MediaNonPublicMetrics],
                        organic_metrics: Option[MediaOrganicMetrics],
                        preview_image_url: Option[String],
                        promoted_metrics: Option[MediaPromotedMetrics],
                        public_metrics: Option[MediaPublicMetrics],
-                       width: Option[Integer])
+                       width: Option[Int])
 
-final case class MediaNonPublicMetrics(playback_0_count: Integer,
-                                       playback_100_count: Integer,
-                                       playback_25_count: Integer,
-                                       playback_50_count: Integer,
-                                       playback_75_count: Integer)
+final case class MediaNonPublicMetrics(playback_0_count: Int,
+                                       playback_100_count: Int,
+                                       playback_25_count: Int,
+                                       playback_50_count: Int,
+                                       playback_75_count: Int)
 
-final case class MediaOrganicMetrics(playback_0_count: Integer,
-                                     playback_100_count: Integer,
-                                     playback_25_count: Integer,
-                                     playback_50_count: Integer,
-                                     playback_75_count: Integer,
-                                     view_count: Integer)
+final case class MediaOrganicMetrics(playback_0_count: Int,
+                                     playback_100_count: Int,
+                                     playback_25_count: Int,
+                                     playback_50_count: Int,
+                                     playback_75_count: Int,
+                                     view_count: Int)
 
-final case class MediaPromotedMetrics(playback_0_count: Integer,
-                                      playback_100_count: Integer,
-                                      playback_25_count: Integer,
-                                      playback_50_count: Integer,
-                                      playback_75_count: Integer,
-                                      view_count: Integer)
+final case class MediaPromotedMetrics(playback_0_count: Int,
+                                      playback_100_count: Int,
+                                      playback_25_count: Int,
+                                      playback_50_count: Int,
+                                      playback_75_count: Int,
+                                      view_count: Int)
 
-final case class MediaPublicMetrics(view_count: Integer)
+final case class MediaPublicMetrics(view_count: Int)
